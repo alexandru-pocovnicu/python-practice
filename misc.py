473264429a4c94167afce4e82fe0a617fddd2fb8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-02 14:48:56 +0100

    added random_number=random.randint(1,100) inside the loop

--- misc.py
+++ misc.py
@@ -1,8 +1,9 @@
 import random
 
 def main():
-    random_number=random.randint(1,100)
+    
     for i in range(10):
+        random_number=random.randint(1,100)
         print (random_number)
 if __name__ == '__main__':
     main()
